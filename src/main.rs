17170d0f837dc8aff2aaa3e3d7b97309ec69a11f
asdfman <aasdfman@proton.me>
2025-05-05 19:09:15 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::ExitCode;
 
-fn main() {
+fn main() -> ExitCode {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -10,6 +11,15 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        println!("{}: command not found", input.trim());
+        let mut input_parts = input.split_whitespace();
+        match input_parts.next().unwrap_or("").to_lowercase().as_str() {
+            "exit" => {
+                if let Some(val) = input_parts.next() {
+                    let num = val.parse::<u8>().unwrap_or(0);
+                    return ExitCode::from(num);
+                }
+            }
+            _ => println!("{}: command not found", input.trim()),
+        }
     }
 }
