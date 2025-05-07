15129abb09131f42bfcbd5139cdcba6742459e77
asdfman <aasdfman@proton.me>
2025-05-07 17:06:16 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,19 @@ fn main() -> ExitCode {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let mut input_parts = input.split_whitespace();
-        match input_parts.next().unwrap_or("").to_lowercase().as_str() {
+        let mut input_parts = input.split_ascii_whitespace();
+        let Some(command) = input_parts.next() else {
+            continue;
+        };
+        match command.to_ascii_lowercase().as_str() {
             "exit" => {
                 if let Some(val) = input_parts.next() {
                     let num = val.parse::<u8>().unwrap_or(0);
                     return ExitCode::from(num);
                 }
+            }
+            "echo" => {
+                println!("{}", input_parts.collect::<Vec<_>>().join(" "))
             }
             _ => println!("{}: command not found", input.trim()),
         }
