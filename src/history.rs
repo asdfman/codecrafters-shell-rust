e37a40b21a173c4bfa3805836c771548ef563ab8
asdfman <aasdfman@proton.me>
2025-08-30 13:37:57 +0300

    codecrafters submit [skip ci]

--- src/history.rs
+++ src/history.rs
@@ -1,12 +1,22 @@
-use std::sync::Mutex;
+use std::{collections::VecDeque, sync::Mutex};
 
 use once_cell::sync::Lazy;
 
 use crate::context::CommandContext;
 
-#[derive(Debug, Default)]
+const MAX_HISTORY_RETAINED: usize = 100;
+
+#[derive(Debug)]
 pub struct CommandHistory {
-    data: Vec<String>,
+    data: VecDeque<String>,
+}
+
+impl Default for CommandHistory {
+    fn default() -> Self {
+        Self {
+            data: VecDeque::with_capacity(MAX_HISTORY_RETAINED),
+        }
+    }
 }
 
 static COMMAND_HISTORY: Lazy<Mutex<CommandHistory>> =
@@ -15,12 +25,21 @@ static COMMAND_HISTORY: Lazy<Mutex<CommandHistory>> =
 impl CommandHistory {
     pub fn add(command: &str) {
         let mut history = COMMAND_HISTORY.lock().unwrap();
-        history.data.push(command.to_string());
+        if history.data.len() == MAX_HISTORY_RETAINED {
+            history.data.pop_front();
+        }
+        history.data.push_back(command.to_string());
     }
 
     pub fn handle_command(ctx: &CommandContext) {
         let history = COMMAND_HISTORY.lock().unwrap();
-        for (index, command) in history.data.iter().enumerate() {
+        let skip_count = ctx
+            .args
+            .first()
+            .and_then(|n| n.parse::<usize>().ok())
+            .map(|n| history.data.len() - n)
+            .unwrap_or(0);
+        for (index, command) in history.data.iter().enumerate().skip(skip_count) {
             let _ = ctx.writeln(format_args!("  {}  {}", index + 1, command));
         }
     }
